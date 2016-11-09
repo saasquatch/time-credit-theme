3247b0cc6bde3183960551e401b3d5b01898e049
jericmason <eric.mason@yupiq.com>
2016-11-09 14:39:50 -0800

    EM - added morp dependency

---
+++ assets/javascript/cta-widget.js
@@ -0,0 +1,15 @@
+(function() {
+  'use strict';
+
+  domready(function() {
+    // CTA
+    each(document.getElementsByClassName('cta-container'), function(el) {
+      el.onclick = function(e) {
+        if (window.frameElement && window.frameElement.squatchJsApi) {
+          window.frameElement.squatchJsApi.open();
+        }
+      }
+    });
+  });
+
+})();
